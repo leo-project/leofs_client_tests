bb10a3add62f3f00435da8775370c899ba499678
mocchira <yoshiyuki.kanno@stoic.co.jp>
2013-11-14 05:07:34 +0000

    Add tests for s3cmd

---
+++ s3cmd/s3cmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+which s3cmd && \
+    s3cmd mb s3://s3cmd-bucket && \
+    s3cmd put README s3://s3cmd-bucket/README && \
+    s3cmd ls s3://s3cmd-bucket && \
+    s3cmd get s3://s3cmd-bucket/README README.copy && \
+    diff README README.copy && rm README.copy && \
+    s3cmd cp s3://s3cmd-bucket/README s3://s3cmd-bucket/README.copy && \
+    s3cmd mv s3://s3cmd-bucket/README.copy s3://s3cmd-bucket/README.org && \
+    s3cmd del s3://s3cmd-bucket/README s3://s3cmd-bucket/README.org && \
+    s3cmd ls s3://s3cmd-bucket && \
+    s3cmd rb s3://s3cmd-bucket
+
