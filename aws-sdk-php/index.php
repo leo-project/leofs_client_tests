483001f46e7264e5378d5b8c34e2d59f29d72716
Paras Patel <parasprince2001@gmail.com>
2014-02-03 19:10:18 +0900

    Update index.php

--- aws-sdk-php/index.php
+++ aws-sdk-php/index.php
@@ -1,66 +1,74 @@
 <?php
 require "vendor/autoload.php";
-
 use Aws\Common\Enum\Region;
 use Aws\S3\S3Client;
 
-// your key==> access_key_id  secret ==> your secret_key_id 
+/* key ==> replace your Access_ID secret ==> replace your secret_key base_url ==> your leofs service address */
 
 $client = S3Client::factory(array(
   "key" => "05236",
   "secret" => "802562235",
   "region" => Region::US_EAST_1,
   "scheme" => "http",
+  'base_url' => 'http://localhost:8080'
+));
+try {
+echo "Bucket List\n";
+echo "------------\n\n";
+// list buckets
+$buckets = $client->listBuckets()->toArray();
+foreach($buckets as $bucket){
+  print_r($bucket);
+}
+print("\n\n");
+echo "Create New Bucket\n\n";
+// create bucket
+$result = $client->createBucket(array(
+  "Bucket" => "test"
 ));
 
-try {
-
-// list buckets
-	$buckets = $client->listBuckets()->toArray();
-
-	foreach($buckets as $bucket)
-	{
-	  print_r($bucket);
-	}
-	print("\n\n");
-
-// create bucket  "Bucket" => "YOUR_BUCKET_NAME"
-	$result = $client->createBucket(array(
-	  "Bucket" => "bucket2"
-	));
+echo "Put object into Bucket \n\n";
 
 // PUT object
-	$client->putObject(array(
-	  "Bucket" => "test",
-	  "Key" => "key-test",
-	  "Body" => "Hello, world!"
-	));
+$client->putObject(array(
+  "Bucket" => "test",
+  "Key" => "key-test",
+  "Body" => "Hello, world!"
+));
 
+echo "Get object from Bucket";
 // GET object
-	$object = $client->getObject(array(
-	  "Bucket" => "test",
-	  "Key" => "key-test"
-	));
-	print($object->get("Body"));
-	print("\n\n");
+$object = $client->getObject(array(
+  "Bucket" => "test",
+  "Key" => "key-test"
+));
+print($object->get("Body"));
+print("\n\n");
 
+echo "Head object\n\n " ;
 // HEAD object
-	$headers = $client->headObject(array(
-	  "Bucket" => "test",
-	  "Key" => "key-test"
-	));
-	print_r($headers->toArray());
+$headers = $client->headObject(array(
+  "Bucket" => "test",
+  "Key" => "key-test"
+));
+print_r($headers->toArray());
 
+echo "delete object \n\n";
 // DELETE object
-	$client->deleteObject(array(
-	  "Bucket" => "test",
-	  "Key" => "key-test"
-	));
+$client->deleteObject(array(
+  "Bucket" => "test",
+  "Key" => "key-test"
+));
+echo "delete bucket \n\n";
+// delete bucket
+$result = $client->deleteBucket(array(
+  "Bucket" => "test"
+));
+echo " object deleted \n\n ";
 }
-catch (\Aws\S3\Exception\S3Exception $e) 
+catch (\Aws\S3\Exception\S3Exception $e)
 {
-// The bucket couldn't be created
-	 echo $e->getMessage();
+// Exeception messages 
+         echo $e->getMessage();
 }
-
 ?>
