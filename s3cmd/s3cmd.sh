fc8b813aaea718185ab793fcf5def307786d3c4e
mocchira <nekotaroh@gmail.com>
2014-10-29 14:30:58 +0900

    Add a s3cmd test case for deleting multi layered dir

--- s3cmd/s3cmd.sh
+++ s3cmd/s3cmd.sh
@@ -12,5 +12,10 @@ which s3cmd && \
     s3cmd mv s3://s3cmd-bucket/README.copy s3://s3cmd-bucket/README.org && \
     s3cmd del s3://s3cmd-bucket/README s3://s3cmd-bucket/README.org && \
     s3cmd ls s3://s3cmd-bucket && \
+    s3cmd put README s3://s3cmd-bucket/a/b/c/README && \
+    s3cmd put s3cmd.sh s3://s3cmd-bucket/a/b/c/s3cmd.sh && \
+    s3cmd ls s3://s3cmd-bucket && \
+    s3cmd del s3://s3cmd-bucket/a && \
+    s3cmd ls s3://s3cmd-bucket && \
     s3cmd rb s3://s3cmd-bucket
 
