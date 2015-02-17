38657e1c8fc1ed65513ab6685a30b94a9a9c7252
mocchira <nekotaroh@gmail.com>
2015-02-17 05:37:36 +0000

    Add a not found test case for s3cmd

--- s3cmd/s3cmd.sh
+++ s3cmd/s3cmd.sh
@@ -7,6 +7,7 @@ which s3cmd && \
     s3cmd put s3cmd-bucket s3://s3cmd-bucket/s3cmd-bucket && \
     s3cmd ls s3://s3cmd-bucket && \
     s3cmd get s3://s3cmd-bucket/README README.copy && \
+    s3cmd get s3://s3cmd-bucket/README.nonexist README.nonexist;test $? -eq 1 && \
     diff README README.copy && rm README.copy && \
     s3cmd cp s3://s3cmd-bucket/README s3://s3cmd-bucket/README.copy && \
     s3cmd mv s3://s3cmd-bucket/README.copy s3://s3cmd-bucket/README.org && \
