3d180b7b1b20d04fa5dfd066c850f1c1db995ee8
Kunal Tyagi <tyagi.kunal@live.com>
2017-10-02 11:05:15 +0900

    Initial setup for connecting to Server

--- aws-sdk-cpp/src/main.cpp
+++ aws-sdk-cpp/src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 #include <aws/core/Aws.h>
 
-int main()
+#include "definitions.h"
+
+int main(int argc, char** argv)
 {
     Aws::SDKOptions options;
     // set the options
@@ -11,7 +14,26 @@ int main()
     std::cout << "=== AWS API Init: Start===\n";
     Aws::InitAPI(options);
     std::cout << "=== AWS API Init: Success ===\n";
+    // setup
+    std::string signVer = SIGN_VER, host = HOST, portStr = PORT, bucket = BUCKET;
+    if (argc)
+    {
+        signVer = argv[1];
+        host = argv[2];
+        portStr = argv[3];
+        bucket = argv[4];
+    }
+    int port = std::stoi(portStr);
+
+    Aws::ClientConfiguration config;
+    config.region = "us-west-2";
+    config.endpointOverride = "http://" + host + ":" + port;
+    config.scheme = Aws::Http::Scheme::HTTP;
+    Aws::Auth::AWSCredentials cred(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+    Aws::S3::S3Client client(cred, config);
     // call tests here
+
+    std::cout << SMALL_TEST_FILE << std::endl;
 
     // end of tests
     std::cout << "=== AWS API Shutdown: Start===\n";
