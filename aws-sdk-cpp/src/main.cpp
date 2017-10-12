87d4c2d8afef7ef9802feba64a44ac579c6ef43b
Kunal Tyagi <tyagi.kunal@live.com>
2017-10-12 18:09:40 +0900

    Multi-part upload in progress
    :)
    Error habndling is more complicated that it looks like

--- aws-sdk-cpp/src/main.cpp
+++ aws-sdk-cpp/src/main.cpp
@@ -5,16 +5,77 @@
 #include <aws/core/Aws.h>
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/s3/S3Client.h>
+#include <aws/s3/model/CompleteMultipartUploadRequest.h>
 #include <aws/s3/model/CreateBucketRequest.h>
+#include <aws/s3/model/CreateMultipartUploadRequest.h>
 #include <aws/s3/model/DeleteBucketRequest.h>
 #include <aws/s3/model/HeadBucketRequest.h>
 #include <aws/s3/model/HeadObjectRequest.h>
+#include <aws/s3/model/ListMultipartUploadsRequest.h>
+#include <aws/s3/model/MultipartUpload.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include <aws/s3/model/UploadPartRequest.h>
 
 #include "definitions.h"
 
 using ClientPtrType = std::shared_ptr<Aws::S3::S3Client>;
 using String = Aws::String;
+using Map = Aws::Map<String, String>;
+
+ClientPtrType init(String host, String port);
+bool doesBucketExists(ClientPtrType client, String bucketName);
+void createBucket(ClientPtrType client, String bucketName);
+void deleteBucket(ClientPtrType client, String bucketName);
+bool doesObjectExists(ClientPtrType client, String bucketName, String key);
+void putObject(ClientPtrType client, String bucketName, String key, String path, Map metadata = Map());
+void putObjectMp(ClientPtrType client, String bucketName, String key, String path, Map metadata = Map());
+
+int main(int argc, char** argv)
+{
+    std::cout << "=== AWS API Init: Start===\n";
+    Aws::SDKOptions options;
+    // set the options
+    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+    // end of options
+    Aws::InitAPI(options);
+    // setup
+    String signVer = SIGN_VER, host = HOST, portStr = PORT,
+                bucketName = BUCKET;
+    if (argc == 5)
+    {
+        signVer = argv[1];
+        host = argv[2];
+        portStr = argv[3];
+        bucketName = argv[4];
+    }
+
+    auto client = init(host, portStr);
+    std::cout << "=== AWS API Init: End ===\n";
+    // call tests here
+    createBucket(client, bucketName);
+    std::cout << '\n';
+
+    // put object
+    putObject(client, bucketName, "test.simple", SMALL_TEST_FILE);
+    putObject(client, bucketName, "test.medium", MED_TEST_FILE);
+    putObject(client, bucketName, "test.large", LARGE_TEST_FILE);
+
+    // put object with metadata
+    Map metadata;
+    metadata[METADATA_KEY] = METADATA_VAL;
+    putObject(client, bucketName, "test.simple.meta", SMALL_TEST_FILE, metadata);
+    putObject(client, bucketName, "test.medium.meta", MED_TEST_FILE, metadata);
+    putObject(client, bucketName, "test.large.meta", LARGE_TEST_FILE, metadata);
+
+    // put object in parts
+
+    deleteBucket(client, bucketName);
+    // end of tests
+    std::cout << "=== AWS API Shutdown: Start===\n";
+    Aws::ShutdownAPI(options);
+    std::cout << "=== AWS API Shutdown: End ===\n";
+    return 0;
+}
 
 ClientPtrType init(String host, String port)
 {
@@ -87,7 +148,7 @@ bool doesObjectExists(ClientPtrType client, String bucketName, String key)
     return false;
 }
 
-void putObject(ClientPtrType client, String bucketName, String key, String path)
+void putObject(ClientPtrType client, String bucketName, String key, String path, Map metadata)
 {
     String base = "=== Put Object [" + bucketName + "/" + key;
     std::cout << base << "]: Start ===\n";
@@ -95,7 +156,18 @@ void putObject(ClientPtrType client, String bucketName, String key, String path)
     auto inpData = Aws::MakeShared<Aws::FStream>("PutObjectInputStream",
             path.c_str(), std::ios_base::in | std::ios_base::binary);
     auto objReq = Aws::S3::Model::PutObjectRequest();
-    objReq.WithBucket(bucketName).WithKey(key).SetBody(inpData);
+    objReq.WithBucket(bucketName).WithKey(key).SetBody(inpData);;
+    if (!metadata.empty())
+    {
+        std::cout << "Key\t:\t\tValue\n";
+        for(auto& it: metadata)
+        {
+            auto key = it.first;
+            auto value = it.second;
+            std::cout << key << "\t:\t\t" << value << "\n";
+        }
+        objReq.SetMetadata(metadata);
+    }
     auto objRes = client->PutObject(objReq);
     if (!objRes.IsSuccess())
     {
@@ -110,40 +182,98 @@ void putObject(ClientPtrType client, String bucketName, String key, String path)
     std::cout << base << "]: End ===\n";
 }
 
-int main(int argc, char** argv)
+void putObjectMp(ClientPtrType client, String bucketName, String key, String path, Map metadata)
 {
-    std::cout << "=== AWS API Init: Start===\n";
-    Aws::SDKOptions options;
-    // set the options
-    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
-    // end of options
-    Aws::InitAPI(options);
-    // setup
-    String signVer = SIGN_VER, host = HOST, portStr = PORT,
-                bucketName = BUCKET;
-    if (argc == 5)
+    String base = "=== Put Object MultiPart [" + bucketName + "/" + key;
+    std::cout << base << "]: Start ===\n";
+    std::cout << "Reading from " << path << "\n";
+    auto inpData = Aws::MakeShared<Aws::FStream>("PutObjectMpInputStream",
+            path.c_str(), std::ios_base::in | std::ios_base::binary);
+    auto objReq = Aws::S3::Model::CreateMultipartUploadRequest();
+    objReq.WithBucket(bucketName).WithKey(key);
+    if (!metadata.empty())
     {
-        signVer = argv[1];
-        host = argv[2];
-        portStr = argv[3];
-        bucketName = argv[4];
+        std::cout << "Map Key\t:\t\tData Value\n";
+        for(auto& it: metadata)
+        {
+            auto key = it.first;
+            auto value = it.second;
+            std::cout << key << "\t:\t\t" << value << "\n";
+        }
+        objReq.SetMetadata(metadata);
     }
-
-    auto client = init(host, portStr);
-    std::cout << "=== AWS API Init: End ===\n";
-    // call tests here
-    createBucket(client, bucketName);
-    std::cout << '\n';
-
-    // put object
-    putObject(client, bucketName, "test.simple", SMALL_TEST_FILE);
-    putObject(client, bucketName, "test.medium", MED_TEST_FILE);
-    putObject(client, bucketName, "test.large", LARGE_TEST_FILE);
-
-    deleteBucket(client, bucketName);
-    // end of tests
-    std::cout << "=== AWS API Shutdown: Start===\n";
-    Aws::ShutdownAPI(options);
-    std::cout << "=== AWS API Shutdown: End ===\n";
-    return 0;
+    auto objRes = client->CreateMultipartUpload(objReq);
+    if (!objRes.IsSuccess())
+    {
+        std::cout << base << "]: Client Side failure ===\n";
+        std::cout << "Couldn't create multi-part upload\n";
+        std::cout << objRes.GetError().GetExceptionName() << "\t" <<
+                     objRes.GetError().GetMessage() << "\n";
+        goto ReturnPt;
+    }
+    {
+    // check if multipart upload exists
+    auto listReq = Aws::S3::Model::ListMultipartUploadsRequest();
+    listReq.WithBucket(bucketName).WithPrefix(key);
+    auto listRes = client->ListMultipartUploads(listReq);
+    if (!listRes.IsSuccess())
+    {
+        std::cout << base << "]: Client Side failure ===\n";
+        std::cout << "Couldn't list multi-part upload\n";
+        std::cout << listRes.GetError().GetExceptionName() << "\t" <<
+                     listRes.GetError().GetMessage() << "\n";
+        // abort upload?
+        goto ReturnPt;
+    }
+    auto id = objRes.GetResult().GetUploadId();
+    auto uploads = listRes.GetResult().GetUploads();
+    if (uploads.size() != 1 || uploads[0].GetUploadId() != id)
+    {
+        std::cout << base << "]: Upload check failure ===\n";
+        std::cout << "Couldn't find in multi-part upload list\n";
+        std::cout << listRes.GetError().GetExceptionName() << "\t" <<
+                     listRes.GetError().GetMessage() << "\n";
+        // abort upload?
+        goto ReturnPt;
+    }
+    }
+    // upload part
+    {
+    auto id = objRes.GetResult().GetUploadId();
+    auto uploadReq = Aws::S3::Model::UploadPartRequest();
+    uploadReq.WithBucket(bucketName).WithKey(key).WithUploadId(id);;
+    uploadReq.WithPartNumber(1).SetBody(inpData);
+    auto uploadRes = client->UploadPart(uploadReq);
+    if (!uploadRes.IsSuccess())
+    {
+        std::cout << base << "]: Client Side failure ===\n";
+        std::cout << "Couldn't list multi-part upload\n";
+        std::cout << uploadRes.GetError().GetExceptionName() << "\t" <<
+                     uploadRes.GetError().GetMessage() << "\n";
+        // abort upload?
+        goto ReturnPt;
+    }
+    }
+    // check if multipart exists
+    // finish upload
+    {
+    auto id = objRes.GetResult().GetUploadId();
+    auto compReq = Aws::S3::Model::CompleteMultipartUploadRequest();
+    compReq.WithBucket(bucketName).WithKey(key).WithUploadId(id);
+    auto compRes = client->CompleteMultipartUpload(compReq);
+    if (!compRes.IsSuccess())
+    {
+        std::cout << base << "]: Client Side failure ===\n";
+        std::cout << "Couldn't complete multi-part upload\n";
+        std::cout << compRes.GetError().GetExceptionName() << "\t" <<
+                     compRes.GetError().GetMessage() << "\n";
+        goto ReturnPt;
+    }
+    }
+ReturnPt:
+    if (!doesObjectExists(client, bucketName, key))
+    {
+        std::cout << base << "]: Failed ===\n";
+    }
+    std::cout << base << "]: End ===\n";
 }
